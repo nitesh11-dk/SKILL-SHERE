3f707f117bcd761cd42b1a36c79f135dfa6aba71
Nitesh-DK <nittesh.dk@gmail.com>
2025-05-02 19:53:42 +0530

    done

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://skill-shere.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
